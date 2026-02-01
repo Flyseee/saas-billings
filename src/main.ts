cc37b2df3033a7ce2fb950fae207d8e2bdce2fcb
Анастасия Беляева <Belyaeva-Nastya-D@ya.ru>
2026-02-01 21:21:01 +0300

    add versioning and validation pipes to main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = app.get(ConfigService);
 
